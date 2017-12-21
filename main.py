62db00fb5b0c890713cd5d71413899b353fca187
dinhvankiet124 <dinhvankiet124@gmail.com>
2017-12-21 18:30:15 +0700

    Try again

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from BOVW import *
 from sklearn.svm import SVC
 
 
-clf = SVC(kernel="rbf", C=2.8, gamma=0.0073, cache_size=10000, probability=False, random_state=111)
-feat = cv2.xfeatures2d.SIFT_create(300, edgeThreshold=50, contrastThreshold=0.02)
+clf = SVC(kernel="rbf", C=2.8, gamma=0.0073, cache_size=10000, probability=False)
+# feat = cv2.xfeatures2d.SIFT_create(300, edgeThreshold=50, contrastThreshold=0.02)
+# feat = cv2.xfeatures2d.SURF_create(0, extended=True)
+feat = cv2.ORB_create(patchSize=64, edgeThreshold=10, nlevels=8)
 
 bovw = BOVW(clf,
             feat,
-            n_bags=300,
-            tol=0.1,
+            n_bags=200,
+            tol=0.01,
             is_resample=True,
             is_reuse=False,
-            verbose=True)
+            verbose=True,
+            color=None)
 bovw.fit("images/train/")
 acc = bovw.score("images/test/")
 print(acc)
