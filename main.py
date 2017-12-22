2596136b0ae1a90daecbda3f00eb97b8e8e19c4e
dinhvankiet124 <dinhvankiet124@gmail.com>
2017-12-22 11:44:54 +0700

    Reach 75%

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from BOVW import *
 from sklearn.svm import SVC
 
 
-clf = SVC(kernel="rbf", C=2.8, gamma=0.0073, cache_size=10000, probability=False)
+# clf = SVC(kernel="rbf", C=2.8, gamma=0.0073, cache_size=10000, probability=False)
+clf = SVC(kernel="rbf", C=5, gamma=0.05, cache_size=10000, probability=False)
 feat = cv2.xfeatures2d.SIFT_create(250, edgeThreshold=50, contrastThreshold=0.02)
 # feat = cv2.xfeatures2d.SURF_create(0, extended=True)
 # feat = cv2.ORB_create(patchSize=64, edgeThreshold=10, nlevels=8)
@@ -12,7 +13,7 @@ bovw = BOVW(clf,
             n_bags=250,
             tol=0.01,
             is_resample=True,
-            is_reuse=False,
+            is_reuse=True,
             verbose=True,
             color=None)
 bovw.fit("images/train/")
